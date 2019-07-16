9cee4b54d6a3fab60c3b464485542ac3ba3fbe71
Elijah Lopez <elijahllopezz@gmail.com>
2019-07-16 13:33:01 -0400

    Update after_build.py

--- after_build.py
+++ after_build.py
@@ -1,23 +1,4 @@
-import json
 import zipfile
-
-default_settings = {
-    'previous device': None,
-    'comments': ['Edit only the variables below', 'Restart the program after editing this file!'],
-    'auto update': True,
-    'run on startup': True,
-    'music directories': [],
-    'sample music directories': [
-        'C:/Users/maste/Documents/MEGAsync/Music',
-        'Put in a valid path',
-        'First path is the default directory when selecting a file to play. FOR NOW'
-    ],
-    'playlists': {},
-    'playlists_example': {'NAME': ['PATHS']},
-}
-
-with open('dist/settings.json', 'w') as outfile:
-    json.dump(default_settings, outfile, indent=4)
 
 with zipfile.ZipFile('dist/Portable.zip', 'w') as zf:
     zf.write('dist/Music Caster.exe', 'Music Caster.exe')
@@ -32,6 +13,3 @@ with zipfile.ZipFile('dist/Python Files.zip', 'w') as zf:
     zf.write('requirements.txt')
 
 print('Created dist/Python Files.zip')
-
-# TODO: add a file check to check if versions are same
-print('ARE THE FILES PROPERLY VERSIONED?')
