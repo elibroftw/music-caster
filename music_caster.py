ec26848aeeccce9aadd1b3bcc5193128535f0e5b
Elijah Lopez <elijahllopezz@gmail.com>
2019-07-20 22:57:13 -0400

    Version update

--- music_caster.py
+++ music_caster.py
@@ -33,7 +33,7 @@ mutex = win32event.CreateMutex(None, False, 'name')
 last_error = win32api.GetLastError()
 if last_error == ERROR_ALREADY_EXISTS: sys.exit()  # one instance
 
-CURRENT_VERSION = '3.1.1'
+CURRENT_VERSION = '3.1.0'
 starting_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir('C:/')
 PORT = 2001
