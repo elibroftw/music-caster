3842a9630c2f245991622278ba30ee56e8c38c97
elibroftw <xxelibroxx@gmail.com>
2019-08-09 13:13:52 -0400

    Update after_build.py

--- after_build.py
+++ after_build.py
@@ -3,15 +3,15 @@ import zipfile
 with zipfile.ZipFile('dist/Portable.zip', 'w') as zf:
     zf.write('dist/Music Caster.exe', 'Music Caster.exe')
     zf.write('dist/Updater.exe', 'Updater.exe')
-    zf.write('Icons/default.png', 'images/default.png')
+    zf.write('resources/default.png', 'images/default.png')
 
 print('Created dist/Portable.zip')
 
 with zipfile.ZipFile('dist/Python Files.zip', 'w') as zf:
     zf.write('music_caster.py', 'music_caster.pyw')
     zf.write('updater.py', 'updater.pyw')
-    zf.write('Icons/icon.ico', 'icon.ico')
-    zf.write('Icons/default.png', 'images/default.png')
+    zf.write('resources/music caster.ico', 'icon.ico')
+    zf.write('resources/default.png', 'images/default.png')
     zf.write('requirements.txt')
 
 print('Created dist/Python Files.zip')
