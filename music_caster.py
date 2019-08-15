29aa3740577b8645cb2b1a1d306bc317f8d16239
Elijah Lopez <elijahllopezz@gmail.com>
2019-08-15 12:43:04 -0400

    Handles home app stop command
    also change in tuple comparisons to in set comparisons

--- music_caster.py
+++ music_caster.py
@@ -38,7 +38,7 @@ mutex = win32event.CreateMutex(None, False, 'name')
 last_error = win32api.GetLastError()
 if last_error == ERROR_ALREADY_EXISTS: sys.exit()
 
-CURRENT_VERSION = '4.7.7'
+CURRENT_VERSION = '4.7.8'
 UNFILLED_ICON = b'iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAQAAAD/5HvMAAAABGdBTUEAALGPC/xhBQAAACBjSFJN\nAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN\nRQfjBw4ALiA+kkFLAAACWElEQVRo3u2ZsUsbURzHo2Bc0kkMDoYirRkcpEu7NtAubo7ZPXDo6qaL\nkyUIQtshkE6CkA79C4SqWIiLi5N2iBQ7WgRvUNvGj0OG/n737kLt9d476PuOvx9JPnn3e9/v3b1C\nwcvLy8srSQwR0CHEpi7pEDAUhzPBNq60zYS5Ou5w+kh6lQhwrUADHTgH6mig0DlQqIGErO7spN/1\nQB7IA3kg10DObnk8kAf6b4C44ZxTDmmzSp3JXPkQAF9o8oLh/AD1dcYalTwBAdzQ4lGegAB+sk4p\nT0AA35i3CVRkjClqLPKGI24ToN4x6sSHGGeB3Visw3875PcyRqb5EAN1xoxDp+Ypnwyk7zxzGh3M\n0TWQZhwCFQqMsWtcuEq2uyzkhB22WGE29oMjNI3xHrXlQ1024rB4xS9tAjaNsccmD2OQtObtOvU1\nDYqRL2hG3LtkEwjgM+XILOnxXrefZV95EtlxXRW7j7MBKlGlxhL79Mx3WxGkOdV9n7EPUabBlbFK\n+sJJ9/6RxpH+NFwrfDRmqagCRWbcaytOzXIkWBuq21auPWwlOqgrpGvpS0yr3ktLWcayWqNN1ZPb\nv5lFlh3TMv+pmqWeDBQW5ENTdj60RzUy3nLHbai7SnnRJrMzxgueq05Dxq7qHIlOPUunvpCrRFlZ\npbxob0V99Z7PMDEnZ4OiY0/19kVnRdQXRb2dGqgzOMvEeLMk6luiXpO3a6mBgsFArYQf3hH1KVE/\nTQlkHOBFdSx6VVE/Ubn/W+epgGKOOAecXvEgoV6UryT+EihMPAT28vLy8urrDgm99Mb0O5qlAAAA\nJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTA3LTE0VDAwOjQ2OjMyKzAwOjAwaWwEjwAAACV0RVh0ZGF0\nZTptb2RpZnkAMjAxOS0wNy0xNFQwMDo0NjozMiswMDowMBgxvDMAAAAASUVORK5CYII=\n'
 FILLED_ICON = b'iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAQAAAD/5HvMAAAABGdBTUEAALGPC/xhBQAAACBjSFJN\nAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN\nRQfjBw4ALiA+kkFLAAACxUlEQVRo3u2ZT0hUURSHn0bjxtpIYqCElLNwEW1yWYO1yF3L2fvARVs3\nqRtX2SAIJTFgK0HQRdJeaBSDaePGlYaoYUtD8C3ScvpaKHTOfe8NOu/fQPe3PGec+bz3nN+57z7H\nsbKysrIKEy24VPFIU8dUcWkJwulihay0Qpd/dbLDOUfSq4RL1nI10JfMgaoayMscyNNAQql2dtjv\nWiAL9N8AJdHfFigWoMvscXMAnTUb0G3G2GkioIuz0iDLTQR08acDVJoKyHEch2dsptX2pxyyxwaL\nTFKkOxQpx2tqKfsQAF8p84TWQKhH7KcPdK4DXtETgHSTj9kAAZwyx10fUivvsgIC+M007T6oseyA\nAL7z3IfkJgeUo4NeCozwhk3+hHzXLG3RV6kBH+IWw6wGYm2YRX71WmrYGOljKQDqgH71qWtX7bho\nw/Uhn3zf+IMBwwT2Ux0dDLHrQ+o3rLKW6iyjg1XfxqlaYiruLvPYpsICE9wPRLpO2VfebapLN5Pz\noV1mgrB4YZwfZ42TQKLGWGOeOwFIWsoqL3teatypTyiRM5DKhnu3qyNcCqPjM51GLenynlbZ5TRm\n2TceGB23q8buPZEbjA+onTwFRlkPcBTPQBpS2ffqcWAndh+ikxI/faukN0669y/pSLxMZrj28MFX\nSzk1UOSMm1LPcWcJOTXjxmAtqeyicu3W2K9jAj9cVEgn0pfoU7mnqQA5DuNqjeZVTrZ/Of4LK48t\n5vz/qaqlmhwoDMuHpuRu0NbIG+UtO25GnSrlpnUnd6V3xGOVKcmxqzJyvhcTvGPkSK4Sncoq5aa9\nFfHJyNdcx/VGx5rKrYvMhIiPiPhiZKBq/VkmyptREV8Q8YI8rkUGcusDzYX8cEXEe0V8LyKQ7wWe\nqS2Ry4v4tpr7/3QYCSjgFWedt1fcCInn5JVEg0Be6EtgKysrK6tz/QVPmZ3Bw5RmTgAAACV0RVh0\nZGF0ZTpjcmVhdGUAMjAxOS0wNy0xNFQwMDo0NjozMiswMDowMGlsBI8AAAAldEVYdGRhdGU6bW9k\naWZ5ADIwMTktMDctMTRUMDA6NDY6MzIrMDA6MDAYMbwzAAAAAElFTkSuQmCC\n'
 WINDOW_ICON = b'iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAQAAAD/5HvMAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfjBxIQARbl3afoAAACwElEQVRo3u2ZPUxTURiGH2osS3UhNphADFE6MBgXGbVBB9kcu9OEwZVFYGESGxISJaRJnUhIygBxJ7EQTOrC0gkMKRgcMSTcQVDqdejg+c49t1HuXxPPe7fvu7336fl5v3PPASsrKysrX/VQpI6DG+N1Rp0iPSacfjZjRVGvTfq9rZMcThtJa6ViojguLkUJ9ClxoLoEchIHciSQmopTvu+1QBbovwGKYoJboFCA/qaPuwLostuAbjPNQTcBtddKY2x0E1Bbo9S6CwjgGY24gC444ZBdqsxRYMAXKc1rWnH7kIvLZ8o8IWWEesRR/EDt65hXDBqQbvI+GSAXlwsq3PUgpVhOCsjF5ScLZDxQ08kBubh85bkHqRgdUJo+hsgzyRsa/PJ51BK9IbTSFXzoFhNsGbF2PYN8OT5jHGbNAHXMiLjr2j/PuEBO/ZAPngd+Y1QzgaN4S8c4TQ/SiGaVrXhrWR9bno6TY2k+bCCHfWqsMst9I9J1yp7h3StmaSM6H2qyaMR6oa0fl7SVQKTG2GKFOwYkeZe0ylrUTn1OibSGVNbcOyOWcDGUjo9ktbEkh/eCyG7EUcu+8ECbcU1Rdu8pubHwgDLkyDPFjsFRHA1pXGTfic+Bg/B9KEuJ755Wkh2nuvcP4UgvozHGQdY9YyktCopa4+bFd9xlVE49oxXWkshWhWunwt+OMakgkM6FLw2L3NN4gGBG/GBF5NTpXw4fyGGPiuGfrgv3VgvKhPhoimwHbZucNrzVGbcoVpVqpw1Et6V3ymORKYmyq0qt74Uo9xhPRStlhVWqnfZWic8F3uY661Q6tkVuR8nMKvFJJV4NDFTvXMvU4T2lxFeVeF4s14ICFTsDVXxeXFPiQ0r8MCCQ5wBPv2FPyeWU+L6o+3/iJ4GADEecnU6vbvjE02JL4mpAjv8hsJWVlZUVAL8BFtCPUbUhaGYAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTktMDctMThUMTU6NTg6MTArMDA6MDBEk3wFAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE5LTA3LTE4VDE1OjU4OjEwKzAwOjAwNc7EuQAAAABJRU5ErkJggg=='
@@ -441,7 +441,7 @@ while True:
                     current_pos = mc.status.adjusted_current_time
                     mc.stop()
             mc = None if cast is None else cast.media_controller
-            if playing_status in ('PAUSED', 'PLAYING'):
+            if playing_status in {'PAUSED', 'PLAYING'}:
                 play_file(music_queue[0], position=current_pos, autoplay=False if playing_status == 'PAUSED' else True)
 
     elif menu_item == 'Settings' and not settings_active:
@@ -500,17 +500,17 @@ while True:
             done_queue.clear()
             play_file(music_queue[0])
             tray.Update(menu=menu_def_2, data_base64=FILLED_ICON)
-    elif 'Stop' in (menu_item, keyboard_command): stop()
-    elif 'Next Song' in (menu_item, keyboard_command) or playing_status == 'PLAYING' and time() > song_end:
+    elif 'Stop' in {menu_item, keyboard_command}: stop()
+    elif 'Next Song' in {menu_item, keyboard_command} or playing_status == 'PLAYING' and time() > song_end:
         next_song(from_timeout=time() > song_end)
-    elif 'Previous Song' in (menu_item, keyboard_command): previous()
+    elif 'Previous Song' in {menu_item, keyboard_command}: previous()
     elif menu_item == 'Repeat':
         repeat_setting = change_settings('repeat', not settings['repeat'])
         if notifications_enabled:
             if repeat_setting: tray.ShowMessage('Music Caster', 'Repeating current song')
             else: tray.ShowMessage('Music Caster', 'Not repeating current song')
-    elif 'Resume' in (menu_item, keyboard_command): resume()
-    elif 'Pause' in (menu_item, keyboard_command): pause()
+    elif 'Resume' in {menu_item, keyboard_command}: resume()
+    elif 'Pause' in {menu_item, keyboard_command}: pause()
     elif menu_item == 'Exit':
         tray.Hide()
         with suppress(UnsupportedNamespace):
@@ -526,14 +526,14 @@ while True:
         settings_value = settings_values.get(settings_event)
         # if settings_event != '__TIMEOUT__':
         #     print(settings_event)
-        if settings_event in ('Esc', 'q'):
+        if settings_event in {'Esc', 'q'}:
             settings_active = False
             settings_window.CloseNonBlocking()
-        elif settings_event in ('auto update', 'run on startup', 'notifications'):
+        elif settings_event in {'auto update', 'run on startup', 'notifications'}:
             change_settings(settings_event, settings_value)
             if settings_event == 'run on startup': startup_setting()
             elif settings_event == 'notifications': notifications_enabled = settings_value
-        elif settings_event in ('volume', 'a', 'd') or settings_event.isdigit():
+        elif settings_event in {'volume', 'a', 'd'} or settings_event.isdigit():
             update_slider = False
             delta = 0
             if settings_event.isdigit():
@@ -569,14 +569,16 @@ while True:
     if keyboard_command is not None: keyboard_command = None
     if mc is not None and time() - cast_last_checked > 2:
         with suppress(UnsupportedNamespace):
-            if cast is not None and cast.app_id == 'CC1AD845':
-                mc.update_status()
-                if mc.is_paused and playing_status != 'PAUSED': pause()
-                elif mc.is_playing and playing_status != 'PLAYING': resume()
-                elif not (mc.is_paused or mc.is_playing) and playing_status != 'NOT PLAYING': stop()
-                # TODO: check if playback was scrubbed
-                volume = settings['volume']
-                cast_volume = int(cast.status.volume_level * 100)
-                if volume != cast_volume:
-                    volume = change_settings('volume', cast_volume)
+            if cast is not None:
+                if cast.app_id == 'CC1AD845':
+                    mc.update_status()
+                    if mc.is_paused and playing_status != 'PAUSED': pause()
+                    elif mc.is_playing and playing_status != 'PLAYING': resume()
+                    elif not (mc.is_paused or mc.is_playing) and playing_status != 'NOT PLAYING': stop()
+                    # TODO: check if playback was scrubbed
+                    volume = settings['volume']
+                    cast_volume = int(cast.status.volume_level * 100)  # TODO: remove int
+                    if volume != cast_volume:
+                        volume = change_settings('volume', cast_volume)
+                elif playing_status in {'PAUSED', 'PLAYING'}: stop()
         cast_last_checked = time()
