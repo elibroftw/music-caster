f337a879cd9af0fcc9bd363850bb74242b152842
elibroftw <xxelibroxx@gmail.com>
2019-11-03 20:42:49 -0500

    Update after_build.py

--- after_build.py
+++ after_build.py
@@ -10,7 +10,6 @@ print('Created dist/Portable.zip')
 with zipfile.ZipFile('dist/Python Files.zip', 'w') as zf:
     zf.write('music_caster.py', 'music_caster.pyw')
     zf.write('updater.py', 'updater.pyw')
-    zf.write('PySimpleGUIWx.py')
     zf.write('resources/Music Caster.ico', 'icon.ico')
     zf.write('resources/default.png', 'images/default.png')
     zf.write('requirements.txt')
