57fb7be165f6ac92a9092170e89b42a1f5c67c93
Elijah Lopez <elijahllopezz@gmail.com>
2019-12-08 18:17:39 -0500

    version++

--- music_caster.py
+++ music_caster.py
@@ -39,7 +39,7 @@ from winerror import ERROR_ALREADY_EXISTS
 import zipfile
 from helpers import *
 
-VERSION = '4.17.24'
+VERSION = '4.17.25'
 update_devices = False
 chromecasts = []
 device_names = ['1. Local Device']
