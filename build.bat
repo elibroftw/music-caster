6c0274d1385cd187e23f84a0c61437c8b50567b5
Elijah Lopez <elijahllopezz@gmail.com>
2020-01-26 14:34:24 -0500

    Updated build and changelog

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
+set startTime=%time%
 pyinstaller music_caster.spec && pyinstaller updater.spec && python after_build.py && iscc "Installer Script.iss"
+echo Start Time: %startTime%
+echo Finish Time: %time%
