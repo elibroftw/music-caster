5f45d63be0be61b60a8866b28a9b8723021b79b6
Elijah Lopez <elijahllopezz@gmail.com>
2020-03-31 18:07:04 -0400

    Update Program.cs

--- Music Caster Updater/Music Caster Updater/Program.cs
+++ Music Caster Updater/Music Caster Updater/Program.cs
@@ -21,7 +21,6 @@ namespace Music_Caster_Updater
                 {
                     string dir = Path.GetDirectoryName(entry.FullName);
                     if (dir != "" && !Directory.Exists(dir)) Directory.CreateDirectory(dir);
-                    if (entry.FullName.Contains("Updater")) continue;
                     try
                     {
                         if (File.Exists(entry.FullName)) File.Delete(entry.FullName);
@@ -36,36 +35,28 @@ namespace Music_Caster_Updater
         private static void Download(string url, string outfile)
         {
             using WebClient myWebClient = new WebClient();
-            string myStringWebResource = url;
-            myWebClient.DownloadFile(myStringWebResource, outfile);
+            myWebClient.DownloadFile(url, outfile);
             if (outfile.EndsWith(".zip")) ExtractZip(outfile);
         }
 
         private static List<string> DirectorySearch(string dir)
-        {
+        {   // returns all files in a dir and its subdirs recursively
             List<string> files = new List<string>();
             try
             {
-                foreach (string f in Directory.GetFiles(dir))
-                {
-                    files.Add(Path.GetFileName(f));
-                }
-                foreach (string d in Directory.GetDirectories(dir))
-                {
-                    files.AddRange(DirectorySearch(d));
-                }
+                foreach (string f in Directory.GetFiles(dir)) files.Add(Path.GetFileName(f));
+                foreach (string d in Directory.GetDirectories(dir)) files.AddRange(DirectorySearch(d));
             }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            catch (Exception) { }
             return files;
         }
 
 
-        static void Main(string[] args)
+        static void Main()
         {
-            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            const string releasesURL = @"https://github.com/elibroftw/music-caster/releases";
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);  // Just in case
             Dictionary<string, object> loadedSettings = new Dictionary<string, object>() { { "DEBUG", false } };
 
             if (File.Exists("settings.json"))
@@ -73,53 +64,48 @@ namespace Music_Caster_Updater
                 using StreamReader r = new StreamReader("settings.json");
                 loadedSettings = JsonConvert.DeserializeObject<Dictionary<string, object>>(r.ReadToEnd());
             }
-
             bool debugSetting = (bool)loadedSettings.GetValueOrDefault("DEBUG", false);
-            const string releasesURL = @"https://github.com/elibroftw/music-caster/releases";
 
             HtmlWeb web = new HtmlWeb();
             HtmlDocument htmlDoc = web.Load(releasesURL);
-            HtmlNode releaseEntry = null;
+            HtmlNode latestEntry = null;
             string latestVersion = null;
             foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//div[@class='release-entry']"))
-            {  // get the latest release
+            {   // get the latest valid release entry
                 string releaseType = node.SelectSingleNode(".//span/a").InnerText;
                 if (releaseType == "Latest release")
                 {
                     latestVersion = node.SelectSingleNode(".//a[contains(@class, 'muted-link') and contains(@class, 'css-truncate')]").GetAttributeValue("title", "0.0.0").Substring(1);
-                    releaseEntry = node;
+                    latestEntry = node;
                     break;
                 }
             }
-            if (releaseEntry != null)
+            if (latestEntry != null)
             {
-                HtmlNode details = releaseEntry.SelectSingleNode(".//details[contains(@class, 'details-reset') and contains(@class, 'Details-element')]");
+                HtmlNode details = latestEntry.SelectSingleNode(".//details[contains(@class, 'details-reset') and contains(@class, 'Details-element')]");
                 List<string> downloadLinks = new List<string>();
                 foreach (HtmlNode node in details.SelectNodes(".//a"))
-                {  // get download links
+                {   // collect download links
                     string url = node.GetAttributeValue("href", "");
-                    if (url != "")
-                    {
-                        downloadLinks.Add(url);
-                    }
+                    if (url != "") downloadLinks.Add(url);
                 }
                 string setupDownloadLink = $"https://github.com{downloadLinks[0]}";
                 string portableDownloadLink = $"https://github.com{downloadLinks[1]}";
-                string sourceDownloadLink = $"https://github.com{downloadLinks[downloadLinks.Count - 2]}";
+                string sourceDownloadLink = $"https://github.com{downloadLinks[^2]}";
                 if (debugSetting)
                 {
                     Console.WriteLine($"Latest Version: {latestVersion}");
-                    Console.WriteLine($"Portable: {portableDownloadLink}");
-                    Console.WriteLine($"Installer: {setupDownloadLink}");
-                    Console.WriteLine($"Source: {sourceDownloadLink}");
+                    Console.WriteLine($"Portable:       {portableDownloadLink}");
+                    Console.WriteLine($"Installer:      {setupDownloadLink}");
+                    Console.WriteLine($"Source:         {sourceDownloadLink}");
                 }
                 else if (File.Exists("unins000.exe"))
-                {
+                {   // Was installed using the Installer
                     Download(setupDownloadLink, "MC_Installer.exe");
                     Process.Start("MC_Installer.exe / VERYSILENT / CLOSEAPPLICATIONS / FORCECLOSEAPPLICATIONS / MERGETASKS = \"!desktopicon\"");
                 }
-                else  // portable installation
-                {
+                else
+                {   // portable installation
                     Download(portableDownloadLink, "Portable.zip");
                     Process.Start("\"Music Caster.exe\"");
                 }
