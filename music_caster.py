19be2a21d92e24904d9c775ab63072d9d1057d64
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-15 14:12:34 -0400

    changelog

--- music_caster.py
+++ music_caster.py
@@ -56,7 +56,7 @@ VERSION = '4.39.4'
 MUSIC_CASTER_DISCORD_ID = '696092874902863932'
 EMAIL = 'elijahllopezz@gmail.com'
 UPDATE_MESSAGE = """
-FIX: Music queue (+2)
+FIX: Music queue remove
 """
 # TODO: Refactoring. Move all constants and functions to before the try-except
 # TODO: move static functions to helpers.py
