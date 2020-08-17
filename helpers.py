4cfe52d430b36c9871745b64d98cca3a671e981b
Elijah Lopez <elijahllopezz@gmail.com>
2020-08-16 21:36:17 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -127,11 +127,11 @@ def valid_music_file(file_path):
 def parse_youtube_id(url):
     query = urlparse(url)
     if query.hostname == 'youtu.be': return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
+    if query.hostname in {'www.youtube.com', 'youtube.com'}:
         if query.path == '/watch': return parse_qs(query.query)['v'][0]
         if query.path[:7] == '/embed/': return query.path.split('/')[2]
         if query.path[:3] == '/v/': return query.path.split('/')[2]
-    return None  # invalid url or YouTube url
+    return None  # invalid YouTube url
 
 
 def get_repeat_img_et_tooltip(repeat_setting):
