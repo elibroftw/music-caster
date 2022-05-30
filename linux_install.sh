49a0dc755b4dc19edbe72b7579ba4ef1cc5f4917
Elijah Lopez <elijahllopezz@gmail.com>
2022-05-30 00:18:53 -0400

    Update Linux install

--- linux_install.sh
+++ linux_install.sh
@@ -2,12 +2,15 @@
 # install virtualenv to avoid global dependdency issues
 # cd into our directory
 cd ~/bin/music-caster/src
-echo "creating venv"
-python3 -m venv venv
+echo "(music-caster) creating Python virtual environment"
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
 source venv/bin/activate
+echo "(music-caster) Installing dependencies"
 python -m pip install -r requirements.txt
 # restore
 cd -
+echo "(music-caster) Registering as desktop application"
 mkdir -p ~/.icons
 cp -rf ~/bin/music-caster/resources/favicons/android-chrome-256x256.png ~/Downloads
 mv -f ~/Downloads/android-chrome-256x256.png ~/.icons/music_caster.png
