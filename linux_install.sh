1d4d51e50689db32336fbc6a7284d5e643c8f171
Elijah Lopez <elijahllopezz@gmail.com>
2023-11-11 19:42:06 -0500

    Improve linux icons

--- linux_install.sh
+++ linux_install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # install virtualenv to avoid global dependdency issues
 # cd into our directory
+cd ~/bin/music-caster
+git fetch
+git reset --hard ORIG_HEAD
+cd -
 cd ~/bin/music-caster/src
 echo "(music-caster) Creating Python virtual environment"
 python3 -m pip install virtualenv
@@ -11,7 +15,27 @@ python -m pip install -r requirements.txt
 # restore
 cd -
 echo "(music-caster) Registering as desktop application"
-mkdir -p ~/.icons
-cp -rf ~/bin/music-caster/resources/favicons/android-chrome-256x256.png ~/Downloads
-mv -f ~/Downloads/android-chrome-256x256.png ~/.icons/music_caster.png
+
+# copy icons
+mkdir -p ~/Downloads/music-caster-tmp
+
+# 32x32
+cp -rf ~/bin/music-caster/resources/icons/32x32.png ~/Downloads/music-caster-tmp
+mv -f ~/Downloads/music-caster-tmp/32x32.png ~/.local/share/icons/hicolor/32x32/apps/music_caster.png
+# 128x128
+cp -rf ~/bin/music-caster/resources/icons/128x128.png ~/Downloads
+mv -f ~/Downloads/music-caster-tmp/128x128.png ~/.local/share/icons/hicolor/128x128/apps/music_caster.png
+# 256x256
+cp -rf ~/bin/music-caster/resources/icons/128x128@2x.png ~/Downloads
+mv -f ~/Downloads/music-caster-tmp/128x128@2x.png ~/.local/share/icons/hicolor/256x256/apps/music_caster.png
+# 512x512
+cp -rf ~/bin/music-caster/resources/icons/icon.png ~/Downloads
+mv -f ~/Downloads/music-caster-tmp/icon.png ~/.local/share/icons/hicolor/512x512/apps/music_caster.png
+
+rm -rf ~/Downloads/music-caster-tmp
+
+# install .desktop file
 cp -rf ~/bin/music-caster/music_caster.desktop ~/.local/share/applications
+
+# delete old files
+rm -rf ~/.icons/music_caster.png
