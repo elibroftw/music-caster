af1c0be265005de8774e4874685183ebb7eaf3bf
Elijah Lopez <elijahllopezz@gmail.com>
2024-10-01 20:06:43 -0400

    Add ipv4 WIFI match test case

---
+++ src/test_cases/ipconfig.py
@@ -0,0 +1,164 @@
+IPCONFIG_ELIBROFTW = '''Windows IP Configuration
+
+
+Ethernet adapter Ethernet 3:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :
+
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::536e:5298:cc0b:3007%15
+   IPv4 Address. . . . . . . . . . . : 192.168.56.1
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . :
+
+Wireless LAN adapter Wi-Fi 2:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :
+
+Wireless LAN adapter Wi-Fi 3:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :
+
+Wireless LAN adapter Wi-Fi 4:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . : ht.home
+
+Wireless LAN adapter Wi-Fi:
+
+   Connection-specific DNS Suffix  . : cgocable.net
+   IPv6 Address. . . . . . . . . . . : 2001:1970:46c7:6f00:b2a6:1d41:da88:1d67
+   Temporary IPv6 Address. . . . . . : 2001:1970:46c7:6f00:9dcc:d93a:540a:f506
+   Link-local IPv6 Address . . . . . : fe80::e427:6d89:2e48:2c23%8
+   IPv4 Address. . . . . . . . . . . : 192.168.0.89
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : fe80::d26d:c9ff:fe4d:774%8
+                                       192.168.0.1
+
+Ethernet adapter Bluetooth Network Connection:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :
+'''
+IPCONFIG_ERICCHAN1989 = '''
+    Windows IP Configuration
+
+Unknown adapter NordLynx:
+
+
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : NordLynx Tunnel
+Physical Address. . . . . . . . . :
+DHCP Enabled. . . . . . . . . . . : No
+Autoconfiguration Enabled . . . . : Yes
+Link-local IPv6 Address . . . . . : fe80::e911:84b9:1c8:cded%55(Preferred)
+IPv4 Address. . . . . . . . . . . : 10.5.0.2(Preferred)
+Subnet Mask . . . . . . . . . . . : 255.255.0.0
+Default Gateway . . . . . . . . . : 0.0.0.0
+DNS Servers . . . . . . . . . . . : 103.86.96.100
+103.86.99.100
+NetBIOS over Tcpip. . . . . . . . : Enabled
+
+Ethernet adapter Ethernet:
+
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Realtek Gaming 2.5GbE Family Controller
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : No
+Autoconfiguration Enabled . . . . : Yes
+IPv4 Address. . . . . . . . . . . : 192.168.2.2(Preferred)
+Subnet Mask . . . . . . . . . . . : 255.255.255.0
+Default Gateway . . . . . . . . . : 192.168.2.1
+DNS Servers . . . . . . . . . . . : 1.1.1.1
+8.8.8.8
+NetBIOS over Tcpip. . . . . . . . : Enabled
+
+Unknown adapter OpenVPN Data Channel Offload for NordVPN:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : OpenVPN Data Channel Offload
+Physical Address. . . . . . . . . :
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Unknown adapter Local Area Connection:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : TAP-NordVPN Windows Adapter V9
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Wireless LAN adapter WiFi:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Intel(R) Wi-Fi 6 AX201 160MHz
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Wireless LAN adapter Local Area Connection* 1:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Wireless LAN adapter Local Area Connection* 10:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter #2
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Ethernet adapter Bluetooth Network Connection:
+
+Media State . . . . . . . . . . . : Media disconnected
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Bluetooth Device (Personal Area Network)
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : Yes
+Autoconfiguration Enabled . . . . : Yes
+
+Ethernet adapter vEthernet (Default Switch):
+
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Hyper-V Virtual Ethernet Adapter
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : No
+Autoconfiguration Enabled . . . . : Yes
+Link-local IPv6 Address . . . . . : fe80::8593:9017:f01e:e94c%25(Preferred)
+IPv4 Address. . . . . . . . . . . : 172.29.0.1(Preferred)
+Subnet Mask . . . . . . . . . . . : 255.255.240.0
+Default Gateway . . . . . . . . . :
+DHCPv6 IAID . . . . . . . . . . . : 419435869
+DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-2E-2D-51-14-70-A6-CC-B2-9B-5C
+NetBIOS over Tcpip. . . . . . . . : Enabled
+
+Ethernet adapter vEthernet (WSL (Hyper-V firewall)):
+
+Connection-specific DNS Suffix . :
+Description . . . . . . . . . . . : Hyper-V Virtual Ethernet Adapter #2
+Physical Address. . . . . . . . . : [removed for security purpose]
+DHCP Enabled. . . . . . . . . . . : No
+Autoconfiguration Enabled . . . . : Yes
+Link-local IPv6 Address . . . . . : fe80::d5b0:1ea6:d494:c06f%50(Preferred)
+IPv4 Address. . . . . . . . . . . : 172.21.160.1(Preferred)
+Subnet Mask . . . . . . . . . . . : 255.255.240.0
+Default Gateway . . . . . . . . . :
+DHCPv6 IAID . . . . . . . . . . . : 838866269
+DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-2E-2D-51-14-70-A6-CC-B2-9B-5C
+NetBIOS over Tcpip. . . . . . . . : Enabled
+'''
--- src/test_harness.py
+++ src/test_harness.py
@@ -14,8 +14,10 @@ import pytest
 from b64_images import DEFAULT_ART
 from meta import COVER_MINI, COVER_NORMAL, VERSION
 from shared import get_running_processes, is_already_running
+from test_cases.ipconfig import IPCONFIG_ELIBROFTW, IPCONFIG_ERICCHAN1989
 from utils import (
     IPV4_GENERAL_PATTERN,
+    IPV4_WIFI_PATTERN,
     REPEAT_ALL_IMG,
     REPEAT_OFF_IMG,
     REPEAT_ONE_IMG,
@@ -363,125 +365,17 @@ def test_mac():
     assert get_mac().count(':') == 5
 
 
-def test_ipv4_pattern():
-    test_input = '''
-    Windows IP Configuration
+def test_ipv4_wifi_match():
+    ipconfig_cleaned = clean_ipconfig(IPCONFIG_ELIBROFTW)
+    wifi_match = IPV4_WIFI_PATTERN.findall(ipconfig_cleaned)
+    assert len(wifi_match) > 0
+    assert wifi_match[-1][-1] == '192.168.0.89'
 
-Unknown adapter NordLynx:
 
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : NordLynx Tunnel
-Physical Address. . . . . . . . . :
-DHCP Enabled. . . . . . . . . . . : No
-Autoconfiguration Enabled . . . . : Yes
-Link-local IPv6 Address . . . . . : fe80::e911:84b9:1c8:cded%55(Preferred)
-IPv4 Address. . . . . . . . . . . : 10.5.0.2(Preferred)
-Subnet Mask . . . . . . . . . . . : 255.255.0.0
-Default Gateway . . . . . . . . . : 0.0.0.0
-DNS Servers . . . . . . . . . . . : 103.86.96.100
-103.86.99.100
-NetBIOS over Tcpip. . . . . . . . : Enabled
-
-Ethernet adapter Ethernet:
-
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Realtek Gaming 2.5GbE Family Controller
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : No
-Autoconfiguration Enabled . . . . : Yes
-IPv4 Address. . . . . . . . . . . : 192.168.2.2(Preferred)
-Subnet Mask . . . . . . . . . . . : 255.255.255.0
-Default Gateway . . . . . . . . . : 192.168.2.1
-DNS Servers . . . . . . . . . . . : 1.1.1.1
-8.8.8.8
-NetBIOS over Tcpip. . . . . . . . : Enabled
-
-Unknown adapter OpenVPN Data Channel Offload for NordVPN:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : OpenVPN Data Channel Offload
-Physical Address. . . . . . . . . :
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Unknown adapter Local Area Connection:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : TAP-NordVPN Windows Adapter V9
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Wireless LAN adapter WiFi:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Intel(R) Wi-Fi 6 AX201 160MHz
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Wireless LAN adapter Local Area Connection* 1:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Wireless LAN adapter Local Area Connection* 10:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter #2
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Ethernet adapter Bluetooth Network Connection:
-
-Media State . . . . . . . . . . . : Media disconnected
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Bluetooth Device (Personal Area Network)
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : Yes
-Autoconfiguration Enabled . . . . : Yes
-
-Ethernet adapter vEthernet (Default Switch):
-
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Hyper-V Virtual Ethernet Adapter
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : No
-Autoconfiguration Enabled . . . . : Yes
-Link-local IPv6 Address . . . . . : fe80::8593:9017:f01e:e94c%25(Preferred)
-IPv4 Address. . . . . . . . . . . : 172.29.0.1(Preferred)
-Subnet Mask . . . . . . . . . . . : 255.255.240.0
-Default Gateway . . . . . . . . . :
-DHCPv6 IAID . . . . . . . . . . . : 419435869
-DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-2E-2D-51-14-70-A6-CC-B2-9B-5C
-NetBIOS over Tcpip. . . . . . . . : Enabled
-
-Ethernet adapter vEthernet (WSL (Hyper-V firewall)):
-
-Connection-specific DNS Suffix . :
-Description . . . . . . . . . . . : Hyper-V Virtual Ethernet Adapter #2
-Physical Address. . . . . . . . . : [removed for security purpose]
-DHCP Enabled. . . . . . . . . . . : No
-Autoconfiguration Enabled . . . . : Yes
-Link-local IPv6 Address . . . . . : fe80::d5b0:1ea6:d494:c06f%50(Preferred)
-IPv4 Address. . . . . . . . . . . : 172.21.160.1(Preferred)
-Subnet Mask . . . . . . . . . . . : 255.255.240.0
-Default Gateway . . . . . . . . . :
-DHCPv6 IAID . . . . . . . . . . . : 838866269
-DHCPv6 Client DUID. . . . . . . . : 00-01-00-01-2E-2D-51-14-70-A6-CC-B2-9B-5C
-NetBIOS over Tcpip. . . . . . . . : Enabled
-    '''
-    test_input_cleaned = clean_ipconfig(test_input)
-    matches = IPV4_GENERAL_PATTERN.findall(test_input_cleaned)
+def test_ipv4_general_match():
+    ipconfig_cleane = clean_ipconfig(IPCONFIG_ERICCHAN1989)
+    assert len(IPV4_WIFI_PATTERN.findall(ipconfig_cleane)) == 0
+    matches = IPV4_GENERAL_PATTERN.findall(ipconfig_cleane)
     assert matches[-1] == '192.168.2.2'
 
 
