b596edaa249d9509f19e1e2686a472321e017c63
Elijah Lopez <elijahllopezz@gmail.com>
2024-10-08 18:46:49 -0400

    linux: fix fedora portaudio dependency

--- scripts/fedora-install.sh
+++ scripts/fedora-install.sh
@@ -2,4 +2,4 @@
 PYTHON=$1
 
 echo "Installing Fedora system dependencies"
-sudo dnf install -y "$PYTHON" python-pip "$PYTHON-devel" "$PYTHON-virtualenv" "$PYTHON-tkinter" python3-pyaudio
+sudo dnf install -y "$PYTHON" python-pip "$PYTHON-devel" "$PYTHON-virtualenv" "$PYTHON-tkinter" portaudio-devel redhat-rpm-config
