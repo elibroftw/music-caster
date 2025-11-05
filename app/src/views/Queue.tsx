9c0dde8f44df274ee1f3c786f2ad62336ad1076b
Elijah Lopez <elijahllopezz@gmail.com>
2025-11-05 18:05:14 -0500

    Fix queue scroll to active, fix height without footer, hide menu on right clicks

--- app/src/views/Queue.tsx
+++ app/src/views/Queue.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Flex, Menu, Paper, Skeleton, Stack, Text } from '@mantine/core';
-import { useScrollIntoView, useWindowEvent } from '@mantine/hooks';
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { Button, Flex, Menu, Paper, ScrollArea, Skeleton, Stack, Text } from '@mantine/core';
+import { useWindowEvent } from '@mantine/hooks';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { MusicCasterAPIContext, PlayerStateContext } from '../common/contexts';
 import TrackContextMenu from '../components/TrackContextMenu';
+import classes from './Queue.module.css';
 
 interface MenuOpen {
 	index: number;
@@ -14,10 +15,8 @@ export default function Queue() {
 	const playerState = useContext(PlayerStateContext);
 	const [menuOpen, setMenuOpen] = useState<MenuOpen | null>(null);
 
-	const { scrollIntoView, targetRef, scrollableRef } = useScrollIntoView<HTMLDivElement>({
-		offset: 6,
-		duration: 300,
-	});
+	const viewportRef = useRef<HTMLDivElement>(null);
+	const targetRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		const handler = () => setMenuOpen(null);
@@ -27,6 +26,11 @@ export default function Queue() {
 		}
 	}, []);
 	useWindowEvent('click', () => setMenuOpen(null));
+	useWindowEvent('contextmenu', event => {
+		if (event.clientX !== menuOpen?.x || event.clientY !== menuOpen.y) {
+			setMenuOpen(null);
+		}
+	});
 
 	const api = useContext(MusicCasterAPIContext)!;
 
@@ -65,26 +69,19 @@ export default function Queue() {
 					onClick={() => onTrackClick(index - queuePosition)}
 				>
 					<Flex gap='md' align='center'>
-
 						<Text size='sm' c='dimmed' style={{ minWidth: '2em', textAlign: 'right' }}>
 							{index - queuePosition}
 						</Text>
 						<Text size='sm' fw={500}>{track}</Text>
-
 					</Flex>
-				</Paper>))
+				</Paper>));
 		}, [JSON.stringify(playerState?.queue), queuePosition]);
 
-	const scrollToActive = () =>
-		scrollIntoView({
-			alignment: 'start'
-		});
-
 	useEffect(() => {
-		if (playerState !== null && targetRef.current !== null) {
-			scrollToActive();
+		if (targetRef.current !== null) {
+			viewportRef.current?.scroll({ top: targetRef.current.offsetTop - 10, behavior: 'smooth' });
 		}
-	}, [scrollToActive, JSON.stringify(playerState?.queue), playerState?.queue_position]);
+	}, [JSON.stringify(playerState?.queue), playerState?.queue_position]);
 
 	const onTrackClick = (index: number) => {
 		if (index < 0) {
@@ -96,9 +93,6 @@ export default function Queue() {
 	}
 
 	const handleEditMetadata = () => {
-	};
-
-	const handleSaveMetadata = () => {
 	};
 
 	const handlePlayNext = () => {
@@ -114,8 +108,8 @@ export default function Queue() {
 	};
 
 	return (
-		<Box style={{ height: 'calc(100vh - 140px)' }}>
-			<Paper shadow='sm' p='md' style={{ height: '100%', overflowY: 'scroll' }} ref={scrollableRef}>
+		<ScrollArea className={classes.tab} viewportRef={viewportRef}>
+			<Paper shadow='sm' p='md' >
 				<Stack gap='xs'>
 					<Menu opened={menuOpen !== null} key={JSON.stringify(menuOpen)}>
 						<Menu.Target>
@@ -127,7 +121,7 @@ export default function Queue() {
 									padding: 0,
 									border: 0,
 									left: (menuOpen?.x ?? 0) + 70,
-									top: (menuOpen?.y ?? 0) - 20,
+									top: (menuOpen?.y ?? 0) - 75,
 								}} />
 						</Menu.Target>
 						<TrackContextMenu
@@ -141,6 +135,6 @@ export default function Queue() {
 					{queueRendered}
 				</Stack>
 			</Paper>
-		</Box >
+		</ScrollArea>
 	);
 }
